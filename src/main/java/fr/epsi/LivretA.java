8cc26cafa9dcf05a63264fca711e5ce6efd2e371
nlauvray <noalauvray@gmail.com>
2025-03-17 11:32:18 +0100

    tp_jpa_4_toujours_en_cours

--- src/main/java/fr/epsi/LivretA.java
+++ src/main/java/fr/epsi/LivretA.java
@@ -2,12 +2,20 @@ package fr.epsi;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 
 @Entity
+@PrimaryKeyJoinColumn(name = "id")
 public class LivretA extends Account {
     private double taux;
     @Id
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Account account;
 
     public void setId(Long id) {
         this.id = id;
--- src/main/java/fr/epsi/Virement.java
+++ src/main/java/fr/epsi/Virement.java
@@ -1,15 +1,25 @@
 package fr.epsi;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
-@PrimaryKeyJoinColumn(name = "id")
+@Table(name = "Virement", schema = "banque", catalog = "banque")
 public class Virement extends Operation {
     private String beneficiaire;
     private String remboursement;
 
-    // Getters et Setters
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Operation operation;
+
+
     public String getBeneficiaire() {
         return beneficiaire;
     }
